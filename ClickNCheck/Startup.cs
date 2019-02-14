1421e7cc64723df6e131b24f77c907547a817a92
Xolani-95 <46720359+Xolani-95@users.noreply.github.com>
2019-02-14 22:02:37 +0200

    Update Startup.cs

--- ClickNCheck/Startup.cs
+++ ClickNCheck/Startup.cs
@@ -39,7 +39,7 @@ namespace ClickNCheck
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+       public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -50,14 +50,19 @@ namespace ClickNCheck
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseCors(o => o.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader());
 
             app.UseHttpsRedirection();
-
+            if (Configuration["EnableCORS"] == "True")
+            {
 
             }
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
 
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
