d2d9a4aeb6a0781f1d154cc361caaaacec20b560
Kudzai Muranga <murangakudzai@gmail.com>
2019-02-28 13:02:56 +0200

    added Candidate_JobProfile

---
+++ ClickNCheck/Models/Candidate_JobProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.Models
+{
+    public class Candidate_JobProfile
+    {
+        public int CandidateID { get; set; }
+        public Candidate Candidate { get; set; }
+
+        public int JobProfileID { get; set; }
+        public JobProfile JobProfile { get; set; }
+    }
+}
