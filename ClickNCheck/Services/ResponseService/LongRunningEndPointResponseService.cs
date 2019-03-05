13d274bc511278ccc40a11c2c0af742a47601cf3
Kudzai Muranga <murangakudzai@gmail.com>
2019-03-05 08:13:10 +0200

    Added The LongRunningEndPoint method

---
+++ ClickNCheck/Services/ResponseService/LongRunningEndPointResponseService.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.Services.ResponseService
+{
+    public class LongRunningEndPointResponseService : IResponseService
+    {
+        void IResponseService.Process(object responseObject)
+        {
+            JObject o = (JObject)JToken.FromObject(responseObject);
+
+            //TODO:
+            //saveResult(o["CheckId"], o["CheckStatus"], o["ResultStatus"], o["ResultDescription"], o["ResultFiles"]);
+        }
+    }
+}
