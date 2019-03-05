d71ec8589b6e71c0d7cbaef3d9962aa22924771c
Kudzai Muranga <murangakudzai@gmail.com>
2019-03-05 09:07:12 +0200

    Added the LongRunningEndPoint controller function

--- ClickNCheck/Models/Results.cs
+++ ClickNCheck/Models/Results.cs
@@ -7,6 +7,7 @@ namespace ClickNCheck.Models
 {
     public class Results
     {
+        public int ID { get; set; }
         public int ServicesID { get; set; }
         public Services Services { get; set; }
         public int CheckStatusID {get;set;}
