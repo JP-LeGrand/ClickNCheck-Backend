ad1891aeac703840c44ed3fee0aa54e0ab3a70fe
Joseph <jLegand@retrorabbit.co.za>
2019-03-14 16:26:47 +0200

    Revert "Changed the authentication tokens, accountsid and the from the number, as this account has the link for the web hooks url"
    This reverts commit d6aaaf8ade1d7f7b2ef2fc6c86b7e479fbb1f4f9.

--- ClickNCheck/Services/SMSService.cs
+++ ClickNCheck/Services/SMSService.cs
@@ -12,9 +12,9 @@ namespace ClickNCheck.Services
 {
     public class SMSService
     {
-        readonly string accountSid = "AC7377f313abdd9e003a9bee0b1e523c3f";
-        readonly string authToken = "752c3e3782dbbcf36aaa0d2e77f041d9";
-        readonly PhoneNumber fromNumber = new PhoneNumber("+12019891465");
+        readonly string accountSid = "AC6d8cdd1e99e0b7139a3df9530633b241";
+        readonly string authToken = "79c9bc4cfd2a11ce18d91fcc01bef0af";
+        readonly PhoneNumber fromNumber = new PhoneNumber("+19704328601");
         public void SendSMS(string messageBody, string userNumber)
         {
             //Initialize Client
