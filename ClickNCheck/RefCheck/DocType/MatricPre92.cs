708ddc0851df01cb3898520b27a68a9392d0668d
Tafdav <tvurayayi@retrorabbit.co.za>
2019-03-18 12:55:35 +0200

    NLevel and Academinc Check model and DocTypes

---
+++ ClickNCheck/RefCheck/DocType/MatricPre92.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.RefCheck.DocType
+{
+    public class MatricPost92
+    {
+        public const int SeniorCertificateGrade12_1992_2007 = 11;
+        public const int National_Senior_Certificate_NSC_from_2008 = 21;
+    }
+}
