0b86a3146ee7364e21321957c6d29a96c6a8de68
Xolani Dlamini <xdlamini@retrorabbit.co.za>
2019-03-18 11:47:48 +0200

    "added model classes"

---
+++ ClickNCheck/RefCheck/Models/DLCCheckModel.cs
@@ -0,0 +1,14 @@
+﻿using ClickNCheck.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.RefCheck.Models
+{
+    public class DLCCheckModel
+    {
+        public string CheckType;
+        public Candidate candidate;
+    }
+}
