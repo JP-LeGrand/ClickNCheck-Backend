4025e424496096c6718838853e728890ed44b3a2
Keoabetswe Morake <kmorake@retrorabbit>
2019-03-18 12:32:02 +0200

    Fixed typo on RefCHeckPRCheck

--- ClickNCheck/RefCheck/Models/RefCheckPRCCheck.cs
+++ ClickNCheck/RefCheck/Models/RefCheckPRCCheck.cs
@@ -8,13 +8,13 @@ namespace ClickNCheck.RefCheck.Models
     public class RefCheckPRCCheck
     {
         public string CheckKey { get; set; }
-        public string PasswportNumber { get; set; }
+        public string PassportNumber { get; set; }
         public string PermitNumber { get; set; }
 
-        public RefCheckPRCCheck(string CheckKey, string PasswportNumber, string PermitNumber)
+        public RefCheckPRCCheck(string CheckKey, string PassportNumber, string PermitNumber)
         {
             this.CheckKey = CheckKey;
-            this.PasswportNumber = PasswportNumber;
+            this.PassportNumber = PasswportNumber;
             this.PermitNumber = PermitNumber;
         }
     }
