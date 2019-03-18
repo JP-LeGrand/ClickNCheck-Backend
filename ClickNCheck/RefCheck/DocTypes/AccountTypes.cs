04a096cb704d0c4e96a906a38c45df83be128c85
Mpinane <mmohale@retrorabbit.co.za>
2019-03-18 11:20:50 +0200

    added director membership and bnk verification models and doc types

---
+++ ClickNCheck/RefCheck/DocTypes/AccountTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.RefCheck.DocTypes
+{
+    public static class AccountTypes
+    {
+        public const int NotKnown = 0;
+        public const int Current = 1;
+        public const int Savings = 2;
+        public const int Transmission = 3;
+        public const int Bond = 4;
+        public const int CreditCard = 5;
+        public const int SubscriptionShare = 6;
+    }
+}
---
+++ ClickNCheck/RefCheck/Models/BVCCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.RefCheck.Models
+{
+    public class RefCheckBVCCheck
+    {
+        [Required]
+        [MaxLength(20)]
+        public int AccountNumber { get; set; }
+        [Required]
+        public int AccountTypeCode { get; set; }
+        [Required]
+        [MaxLength(2)]
+        [MinLength(2)]
+        public string BankCode { get; set; }
+        [Required]
+        public string CheckKey { get; set; }
+        [Required]
+        public string IDNumberCompanyRegistrationNumber { get; set; }
+        [MaxLength(2)]
+        public string Initials { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string SurnameBusinessName { get; set; }
+    }
+}
