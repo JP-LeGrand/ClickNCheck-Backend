bd61468e3e28e9d1aa80a41fd34775b32e268b44
Chuba <cgmoanedi@retrorabbit.co.za>
2019-03-18 11:37:09 +0200

    Added employment history model

---
+++ ClickNCheck/RefCheck/Models/RefCheckEmploymentHistoryCheck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.RefCheck.Models
+{
+    public class RefCheckEmploymentHistoryCheck
+    {
+        public string CheckKey { get; set; }
+        public string ContactNumber { get; set; }
+        public string ContactPerson { get; set; }
+        public string Country { get; set; }
+        public bool CurrentlyEmployed { get; set; }
+        public string DepartmentRegionName { get; set; }
+        public string Employer { get; set; }
+        public DateTime EmploymentEndDate { get; set; }
+        public DateTime EmploymentStartDate { get; set; }
+        //public EmploymentTypeCode { get; set; }
+    }
+}
