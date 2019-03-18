bc17208514976da9f9c9e7c3abcc53ba74d649cd
Kudzai Muranga <murangakudzai@gmail.com>
2019-03-18 12:31:27 +0200

    Added models

---
+++ ClickNCheck/RefCheck/Models/RefCheckLexisNexisBatchConfirmationV2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.RefCheck.Models
+{
+    public class RefCheckLexisNexisBatchConfirmationV2
+    {
+        public string SessionKey { get; set; }
+        public string BatchId { get; set; }
+        public bool IsConfirmed { get; set; }
+    }
+}
