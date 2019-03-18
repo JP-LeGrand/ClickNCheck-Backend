50f9bffe03be24298684f6159101b48ee7b2f962
Keoabetswe Morake <kmorake@retrorabbit>
2019-03-18 11:45:13 +0200

    Added Permenancy check model

---
+++ ClickNCheck/RefCheck/Models/RefCheckPRCCheck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickNCheck.RefCheck.Models
+{
+    public class RefCheckPRCCheck
+    {
+        public string CheckKey { get; set; }
+        public string PasswportNumber { get; set; }
+        public string PermitNumber { get; set; }
+
+        public RefCheckPRCCheck(string CheckKey, string PasswportNumber, string PermitNumber)
+        {
+            this.CheckKey = CheckKey;
+            this.PasswportNumber = PasswportNumber;
+            this.PermitNumber = PermitNumber;
+        }
+    }
+}
